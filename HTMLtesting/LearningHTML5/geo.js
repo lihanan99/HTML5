873484f8f93146fbe3d2a1a2c5f9707f5d0e699c
lihanan99 <hli@fandm.edu>
2014-02-07 22:21:34 -0500

    update on map

--- HTMLtesting/LearningHTML5/geo.js
+++ HTMLtesting/LearningHTML5/geo.js
@@ -1,7 +1,12 @@
 /**
 This is a test for geolocation fucntion of the HTML feature
 
+
 @auther Hannan Li
+
+update 1:
+Adding error message
+
 */
 
 window.onload=getMyLocation;
@@ -9,7 +14,7 @@ window.onload=getMyLocation;
 function getMyLocation(){
 	if(navigator.geolocation){
 		//alert("we get some location ~");
-		navigator.geolocation.getCurrentPosition(displayLocation);
+		navigator.geolocation.getCurrentPosition(displayLocation,displayErrorMsg);
 		alert("hi");
 	}else{
 		alert("no geolocation suppoer for this browser ");
@@ -17,12 +22,26 @@ function getMyLocation(){
 
 }
 function displayLocation(position){
-	alert("into display location function ~!");
 	var latitude = position.coords.latitude;
 	var longitude = position.coords.longitude;
 	var div= document.getElementById("showLocation");
 	div.innerHTML="ur latitude is "+latitude+", and your longitude is "+longitude+" .";
+}
+
+function displayErrorMsg(error){
+	var errorType = {
+			0 : "Unkonw error",
+			1 : "Permission error",
+			2 : "Position is not available",
+			3 : "request Time out"
+	};
 	
+	var errorMsg = errorType[error.code];
+	if(error.code==0 || error.code == 2){
+		errorMsg=errorMsg+" "+error.message;
+	}
+	var div = document.getElementById("showLocation");
+	div.innerHTML=errorMsg;
 	
 	
 }
